374408a39f863909770a2115539e07b3cbf4fbf3
Eliorco <eliorc1988@gmail.com>
2020-08-18 18:51:53 +0300

    Added tests for BitManipulation class Adjusted BitOperation class in BitManipulation methods

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
-import os
-
 class BitOperators:
     """
-    Receive decimal number and return decimal number
-    after changing it to binary
-    compute it and
-    return it to decimal number
+    Simple Wrapper that commit bit operation as a function
     """
     @classmethod
     def _not(cls, first: int) -> int:
@@ -23,36 +18,41 @@ class BitOperators:
     def _xor(cls, first: int, second: int) -> int:
         return int(first ^ second)
 
+
 class BitManipulation:
     """
-    Class that gather all major bit manipulation and show it the easiest way
+    Class that gather all basic Bit manipulation and demonstrate it with explained prints
     """
     b = BitOperators()
 
+    @staticmethod
+    def _create_mask(size: int) -> int:
+        return (1 << size)
+
     @classmethod
-    def _get_the_ith_bit(cls, binary_num: int, i: int) -> int:
-        mask = (1 << i)
+    def get_the_ith_bit(cls, binary_num: int, i: int) -> int:
+        mask = cls._create_mask(i)
         print(f'create a mask: move 1 bit i={i} times left(multiply it by 2) --> {bin(mask)[2:]}')
         bit_is_on = (binary_num & mask) != 0
         print(f'Commit AND operator between {bin(mask)[2:]} & {bin(binary_num)[2:]} --> {binary_num & mask}')
         return 1 if bit_is_on else 0
 
     @classmethod
-    def _set_the_ith_bit(cls, binary_num: int, i: int) -> int:
+    def set_the_ith_bit(cls, binary_num: int, i: int) -> int:
         """
 
         :param binary_num: decimal number that need to adjust
         :param i: the index of the wished bit
         :return: decimal number with the new bit
         """
-        mask = (1 << i)
+        mask = cls._create_mask(i)
         print(f'create a mask: move 1 bit i={i} times left(multiply it by 2) --> {bin(mask)[2:]}')
         bit_is_set = (binary_num | mask)
         print(f'Commit OR operator between {bin(mask)[2:]} | {bin(binary_num)[2:]} --> {binary_num | mask}')
         return bit_is_set
 
     @classmethod
-    def _clear_ith_bit(cls, binary_num: int, i: int) -> int:
+    def clear_ith_bit(cls, binary_num: int, i: int) -> int:
         """
         Go to the ith bit and clear it
         :param binary_num: decimal number that need to adjust
@@ -60,7 +60,7 @@ class BitManipulation:
         :return: new decimal number by the cleared bit
         """
         # k = 1 << i
-        mask = -(1 << i) -1 # equivalent to ~(1 << i)
+        mask = cls.b._not(cls._create_mask(i)) # equivalent to ~(1 << i)
         # mask = f'{"0" * (len(bin(23)[2:]) - len(bin(k)[2:]))}{bin(k)[2:]}'
         print(bin(mask))
         print(f'create a mask: move 1 bit i={i} times left(multiply it by 2) and negate it (python show it different) --> {bin(mask)[2:]}')
@@ -69,8 +69,8 @@ class BitManipulation:
         return bit_is_cleared
 
     @classmethod
-    def _clear_MSB_to_i_bits(cls, binary_num: int, i: int) -> int:
-        mask = (1 << i) - 1 # to create sequence of i 1s
+    def clear_MSB_to_i_bits(cls, binary_num: int, i: int) -> int:
+        mask = cls._create_mask(i) - 1 # to create sequence of i 1s
         print(mask)
         print(f'create a mask: move 1 bit i={i} times left(multiply it by 2) --> {bin(mask)[2:]}')
         bit_is_cleared = binary_num & mask
@@ -79,7 +79,7 @@ class BitManipulation:
 
 
     @classmethod
-    def _clear_i_to_LSB_bits(cls, binary_num: int, i: int) -> int:
+    def clear_i_to_LSB_bits(cls, binary_num: int, i: int) -> int:
         mask = (-1 << i + 1)
         print(mask)
         print(f'create a mask: move 1 bit i={i} times left(multiply it by 2) --> {bin(mask)[2:]}')
@@ -88,9 +88,9 @@ class BitManipulation:
         return bit_is_cleared
 
     @classmethod
-    def _update_ith_bit(cls, binary_num: int, i: int, bit: bool = False) -> int:
+    def update_ith_bit(cls, binary_num: int, i: int, bit: bool = False) -> int:
         val = bit
-        mask = ~(1 << i)
+        mask = cls.b._not(cls._create_mask(i))
         print(bin(mask))
         print(f'create a mask: move 1 bit i={i} times left(multiply it by 2) and negate it (python show it different) --> {bin(mask)[2:]}')
         print(f'bit is: {bit}')
---
+++ test_bit_manipulation.py
@@ -0,0 +1,77 @@
+from unittest import TestCase
+from run import BitManipulation
+
+
+class TestBitManipulation(TestCase):
+
+    def setUp(self):
+        self.bm = BitManipulation()
+
+    def test__create_mask(self):
+        """
+        creating simple mask
+        :return: mask with i shifts to the left => '10000'
+        """
+        mask = self.bm._create_mask(4)
+        self.assertEqual(16, mask)
+
+    def test_get_the_ith_bit(self):
+        """    16 8 4 2 1
+        23 =>  '1 0 1 1 1'
+        index:  4 3 2 1 0
+        :return: bit at index 4 => 16 == 1
+        """
+        self.assertEqual(1, self.bm.get_the_ith_bit(23,4))
+
+    def test_set_the_ith_bit(self):
+        """
+        setting the 8 bit to 1
+                  16 8 4 2 1
+        input =>  '1 0 1 1 1'
+        index:     4 3 2 1 0
+        :return: 31  =>  '1 1 1 1 1'
+        """
+        self.assertEqual(31, self.bm.set_the_ith_bit(23, 3))
+
+    def test_clear_ith_bit(self):
+        """
+        clearing the 8 bit to 0
+                  16 8 4 2 1
+        input =>  '1 1 1 1 1'
+        index:     4 3 2 1 0
+        :return: 23  =>  '1 0 1 1 1'
+        """
+        self.assertEqual(23, self.bm.clear_ith_bit(31, 3))
+
+    def test_clear_MSB_to_i_bits(self):
+        """
+        clearing the MSB bit to ith bit to 0, index 4,3
+                  16 8 4 2 1
+        input =>  '1 1 1 1 1'
+        index:     4 3 2 1 0
+        :return: 3  =>  '0 0 0 1 1'
+        """
+        self.assertEqual(3, self.bm.clear_MSB_to_i_bits(31, 2))
+
+    def test_clear_i_to_LSB_bits(self):
+        """
+        clearing the 8 bit to LSB to 0, index 2,1,0
+                  16 8 4 2 1
+        input =>  '1 1 1 1 1'
+        index:     4 3 2 1 0
+        :return: 24  =>  '1 1 0 0 0'
+        """
+        self.assertEqual(24, self.bm.clear_i_to_LSB_bits(31, 2))
+
+    def test_update_ith_bit(self):
+        """
+        updating the 8 bit to 1 and the 2 bit to 0
+                  16 8 4 2 1
+        input =>  '1 0 1 1 1'
+        index:     4 3 2 1 0
+        :return: 31  =>  '1 1 1 1 1'
+        and
+        :return: 21  =>  '1 0 1 0 1'
+        """
+        self.assertEqual(31, self.bm.update_ith_bit(23, 3, True))
+        self.assertEqual(21, self.bm.update_ith_bit(23, 1))
